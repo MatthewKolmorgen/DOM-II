9b61eab3c6b0af670951f10bbda3bb49e4f45396
Matthew <mattk.development@gmail.com>
2020-08-04 13:35:28 -0500

    Added mouseover

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+// Nav Itmes mouseover
+const navItem = document.querySelectorAll("nav a");
+
+navItem.forEach( (element) => {
+    element.addEventListener('mouseover', (event) => {
+        element.style.color = 'purple';
+    });
+});
+
